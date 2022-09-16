6ca75ad2bf9623667c3fae63f2a43cf15205254c
fran-janela <francisco.pinheiro.janela@gmail.com>
2022-09-16 10:35:51 -0300

    correcting mistakes

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': 'tasks',
         'USER': 'cloud',
         'PASSWORD': 'cloud',
-        'HOST': '192.168.0.7',
+        'HOST': '192.168.15.0',
         'PORT': '5432',
     }
 }
