df16c47cd22106e5b4ad733ed99f53ba9789a837
fran-janela <francisco.pinheiro.janela@gmail.com>
2022-10-14 10:04:14 -0300

    atualizando db ip_address

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': 'tasks',
         'USER': 'cloud',
         'PASSWORD': 'cloud',
-        'HOST': '192.168.15.0',
+        'HOST': '192.169.0.78',
         'PORT': '5432',
     }
 }
